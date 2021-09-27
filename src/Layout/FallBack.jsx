88a970753b8f6575f54d29c8cdae0f40ca40b327
Zion98 <adeyemioluwasegun98@gmail.com>
2021-09-27 18:40:05 +0100

    added newsocials

---
+++ src/Layout/FallBack.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FallBack = () => {
+  return (
+    <div>
+      <h1>Something went wrong, Should be rectified shortly</h1>
+    </div>
+  );
+};
+
+export default FallBack;
