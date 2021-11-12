fe66ae27a91882b54bd6e598b8350a10438921a7
yomioakande <akandeabayomi60@gmail.com>
2021-11-12 15:41:35 +0100

    second commit to new branch

--- src/Layout/Group/JoinGroup1.jsx
+++ src/Layout/Group/JoinGroup1.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import NumberFormat from "react-number-format";
 import { connect } from "react-redux";
 import { usersActions } from "../../redux/actions";
